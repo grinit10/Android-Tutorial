3f1ee668a23f9e3cbe0511ea33f52eb05841d09f
Arnab <grinit10@gmail.com>
2022-03-14 20:09:09 +1100

    Adding show hide functionality.

--- TimerExample/app/src/main/java/com/example/timerexample/MainActivity.java
+++ TimerExample/app/src/main/java/com/example/timerexample/MainActivity.java
@@ -5,13 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    public void show(View view) {
+        textView.setVisibility(View.VISIBLE);
+    }
+
+    public void hide(View view) {
+        textView.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textViewThang);
 
         new CountDownTimer(10000, 100) {
 
