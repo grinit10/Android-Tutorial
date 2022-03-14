7c212858bb54ee3743b141a1f172321f4a21f8d2
Arnab <grinit10@gmail.com>
2022-03-14 22:55:38 +1100

    Adding multiple constructors.

--- KotlinExample/app/src/main/java/com/example/kotlinexample/Basics.kt
+++ KotlinExample/app/src/main/java/com/example/kotlinexample/Basics.kt
@@ -1,13 +1,25 @@
 package com.example.kotlinexample
 
 fun main() {
-    MobilePhone("iOS", "Apple", "iPhone 12")
-    MobilePhone("Android", "Samsung", "Galaxy S20")
-    MobilePhone("Android", "Huawei", "Mate X S")
+    val i12 = MobilePhone("iOS", "Apple", "iPhone 12", "2019")
+    val s20 = MobilePhone("Android", "Samsung", "Galaxy S20")
+    val mate = MobilePhone("Android", "Huawei", "Mate X S")
+    i12.displayPhone()
+    s20.displayPhone()
+    mate.displayPhone()
 }
 
 class MobilePhone(osName: String, brand: String, model: String) {
-    init {
-        println("The phone $model from $brand uses $osName as its Operating System")
+    private var releaseYear: String? = null
+    var osName: String = osName
+    var brand: String = brand
+    var model: String = model
+
+    constructor(osName: String, brand: String, model: String, releaseYear: String): this(osName, brand, model) {
+        this.releaseYear = releaseYear;
+    }
+
+    fun displayPhone(){
+        println("The phone $model from $brand uses $osName as its Operating System" + (if (releaseYear != null)  " and is release in $releaseYear" else ""))
     }
 }
