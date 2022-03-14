1007b4c41a15299b6c9ca987ffebb17d2eedf95a
Arnab <grinit10@gmail.com>
2022-03-14 12:27:50 +1100

    Adding audio.

---
+++ SoundExample/app/src/main/java/com/example/soundexample/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.soundexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.test);
+
+        mediaPlayer.start();
+    }
+}
