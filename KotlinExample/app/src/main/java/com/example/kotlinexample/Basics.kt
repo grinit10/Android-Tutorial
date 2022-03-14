305630eb7a6786258b943a69866c7a243054cde0
Arnab <grinit10@gmail.com>
2022-03-14 22:02:52 +1100

    Adding basic class.

--- KotlinExample/app/src/main/java/com/example/kotlinexample/Basics.kt
+++ KotlinExample/app/src/main/java/com/example/kotlinexample/Basics.kt
@@ -1,2 +1,13 @@
 package com.example.kotlinexample
 
+fun main() {
+    MobilePhone("iOS", "Apple", "iPhone 12")
+    MobilePhone("Android", "Samsung", "Galaxy S20")
+    MobilePhone("Android", "Huawei", "Mate X S")
+}
+
+class MobilePhone(osName: String, brand: String, model: String) {
+    init {
+        println("The phone $model from $brand uses $osName as its Operating System")
+    }
+}
