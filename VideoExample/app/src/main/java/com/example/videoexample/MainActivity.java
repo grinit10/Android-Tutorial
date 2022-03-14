450a3650706c9f8394195962dad6497a2b4441bb
Arnab <grinit10@gmail.com>
2022-03-14 12:06:18 +1100

    Finising up the videoplayer.

--- VideoExample/app/src/main/java/com/example/videoexample/MainActivity.java
+++ VideoExample/app/src/main/java/com/example/videoexample/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.videoexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.example);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        videoView.start();
     }
 }
