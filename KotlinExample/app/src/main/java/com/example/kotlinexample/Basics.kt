d144e2515868ced7095354822d245d0ef1b2030d
Arnab <grinit10@gmail.com>
2022-03-15 09:07:30 +1100

    Adding release yers.

--- KotlinExample/app/src/main/java/com/example/kotlinexample/Basics.kt
+++ KotlinExample/app/src/main/java/com/example/kotlinexample/Basics.kt
@@ -1,7 +1,9 @@
 package com.example.kotlinexample
 
+import java.lang.IllegalArgumentException
+
 fun main() {
-    val i12 = MobilePhone("iOS", "Apple", "iPhone 12", "2019")
+    val i12 = MobilePhone("iOS", "Apple", "iPhone 12", 2023)
     val s20 = MobilePhone("Android", "Samsung", "Galaxy S20")
     val mate = MobilePhone("Android", "Huawei", "Mate X S")
     i12.displayPhone()
@@ -10,16 +12,16 @@ fun main() {
 }
 
 class MobilePhone(osName: String, brand: String, model: String) {
-    private var releaseYear: String? = null
+    private var releaseYear: Int? = null
     var osName: String = osName
     var brand: String = brand
     var model: String = model
 
-    constructor(osName: String, brand: String, model: String, releaseYear: String): this(osName, brand, model) {
-        this.releaseYear = releaseYear;
+    constructor(osName: String, brand: String, model: String, releaseYear: Int): this(osName, brand, model) {
+        this.releaseYear = if  ((releaseYear as Int) <2022) releaseYear else throw IllegalArgumentException("Release year must be less than 2022");
     }
 
     fun displayPhone(){
-        println("The phone $model from $brand uses $osName as its Operating System" + (if (releaseYear != null)  " and is release in $releaseYear" else ""))
+        println("The phone $model from $brand uses $osName as its Operating System" + (if (releaseYear != null)  " and is released in $releaseYear" else ""))
     }
 }
